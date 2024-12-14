8fc6175381c9c22f5c2564f29c378390edff05bd
Rikza Kurnia Almujtaba Lubis <rikza.kurnia@ui.ac.id>
2024-12-14 14:13:53 +0700

    init env variable usage on app

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,15 @@
+import os
 from flask import Flask
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def create_app():
     app = Flask(__name__)
+
+    # Setup env
+    app.config['FLASK_APP'] = os.getenv('FLASK_APP')
+    app.config['ENV_EXAMPLE'] = os.getenv('ENV_EXAMPLE')
 
     # Import and register blueprints
     from .routes import main
--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, current_app
 
 main = Blueprint('main', __name__)
 
@@ -9,9 +9,15 @@ def index():
 @main.route("/results", methods=["POST"])
 def results():
     query = request.form.get("query")
+
+    # Using env variable
+    ENV_EXAMPLE = current_app.config['ENV_EXAMPLE']
+
     # Mock results for now
     results = [
         {"title": "Mock Title 1", "snippet": "This is a snippet of result 1."},
         {"title": "Mock Title 2", "snippet": "This is a snippet of result 2."},
+        {"title": ENV_EXAMPLE, "snippet": "This is a snippet of result 2."},
+
     ]
     return render_template("results.html", query=query, results=results)
