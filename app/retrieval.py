75a7e73ddf1ae4b027cadfd9d9bf90513fd2e849
Rikza Kurnia Almujtaba Lubis <rikza.kurnia@ui.ac.id>
2024-12-22 10:59:15 +0700

    add more dependency

---
+++ app/retrieval.py
@@ -0,0 +1,12 @@
+import re
+import pyterrier as pt
+import pandas as pd
+import numpy as np
+import contractions
+import zipfile
+import os
+import pickle
+import xgboost as xgb
+from gensim.models import Doc2Vec
+from pyterrier.measures import *
+
